3b09cef9619f3956676f2e340c81d39f77b968d9
DylanAaronElicserioMartinez <a.23308051281179@cbtis128.edu.mx>
2024-10-01 19:58:31 -0600

    Add files via upload

---
+++ PR9_Manejo_de_Tuplas/pr9_manejo_de_tuplas.py
@@ -0,0 +1,49 @@
+print(" ")#imprime un espacio vacio
+print("Dylan Aaron Elicserio Martínez 3°W")#imprime mi nombre y mi grado
+print(" ")#imprime un espacio vacio
+thistuple = ("apple", "banana", "cherry")#crea una lista tuple
+print(thistuple)#imprime la lista
+print(" ")#imprime un espacio vacio
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")#crea una lista tuple
+print(thistuple)#imprime la lista
+print(" ")#imprime un espacio vacio
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))#imprime la lista
+print(" ")#imprime un espacio vacio
+thistuple = ("apple",)#crea una lista tuple
+print(type(thistuple))#imprime la lista de forma de tipo
+
+#NOT a tuple
+thistuple = ("apple")#crea una lista tuple
+print(type(thistuple))#imprime la lista de forma de tipos
+print(" ")#imprime un espacio vacio
+#Crear Tupla 
+thistuple = ("pizza", "hot dog", "hamburgesa")#crea una lista tuple
+print(thistuple)#imprime la lista
+print(" ")#imprime un espacio vacio
+#utilizamos el metodo tuple para hacer una lista de las peliculas de spiderman
+thistuple = tuple(("spiderman 1", "spiderman 2", "spiderman 3", "amazing spiderman 1", "amazing spiderman 2"))#crea una lista tuple
+print("estas son las mejores peliculas de spiderman:")
+print(thistuple)#imprime la lista
+print(" ")#imprime un espacio vacio
+#le pusimos el valor de la liga de la justicia a turple4
+tuple1 = ("batman", "superman", "la mujer maravilla", "acuaman", "flash", "cybord", "detective marciano", "linterna verde", "hombre alcon")#crea una lista tuple numerada
+print("esta es la liga de la justicia:")
+print(tuple1)#imprime la lista
+print(" ")#imprime un espacio vacio
+#vamos a utilizar una lista turple para hacer la mencion de los anillos de las linternas 
+print("estas son las linternas de poder que conosco solo que no se si exista la negra:")
+tuple2 = ("linterna verde", "linterna roja", "linterna naranja", "linterna azul", "linterna rosa", "linterna morada", "linterna blanca", "linterna negra?")#crea una lista tuple numerada
+print(tuple2)#imprime la lista
+print(" ")#imprime un espacio vacio
+#estos son los mejores futbolistas del momento actual utilizando thistuple
+print("los mejores futbolistas del momento:")
+thistuple = ("messi 10", "cristiano 7", "Halaand 9")#crea una lista tuple
+print(thistuple)#imprime la lista
+print(" ")#imprime un espacio vacio
+#Tupla con miembros duplicados 
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")#crea una lista tuple
+print(thistuple)#imprime la lista
+print(" ")#imprime un espacio vacio
+mytuple = ("manzana", "banana", "cereza")#crea mi lista tuple
+print(type(mytuple))#imprime la lista en forma de tipos
